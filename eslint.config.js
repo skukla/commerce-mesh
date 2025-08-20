7be286bb645d0ae097a9034d4629625e98b90bdd
Steve Kukla <stevenjkukla@gmail.com>
2025-08-20 11:16:28 -0400

    chore: add Prettier and pre-commit hooks
    - Add Prettier with ideal configuration
    - Set up ESLint v9 with flat config format
    - Set up Husky for git hooks
    - Add lint-staged to run checks on staged files only
    - Add format and lint scripts to package.json
    - Remove old .eslintrc.json in favor of new eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'build/**', 'mesh.json'],
+  },
+];
